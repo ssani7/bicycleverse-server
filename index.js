24450d743f7e11f85183ad0e444bef6fb7a72e32
ssani7 <sanaullah.sani756@gmail.com>
2022-06-11 13:52:58 -0600

    bug solved:use admin jwt removed

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ const run = async () => {
 
         // admin features
 
-        app.get('/admin/:email', verifyJWT, async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await usersCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
